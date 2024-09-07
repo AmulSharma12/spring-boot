b77d3ec5a3940a64ff0b8868eed8b19faf15215a
Amul Op <amulsharmarr67@gmail.com>
2024-09-07 13:51:03 +0530

    Spring boot - Setter Injection

--- Spring Boot/SpringCoreConcepts/src/main/java/com/springcore/Coupling/Person.java
+++ Spring Boot/SpringCoreConcepts/src/main/java/com/springcore/Coupling/Person.java
@@ -10,16 +10,36 @@ public class Person {
 
 
    //constructor injection preferred when the dependency is must  or compulsory need to inject
-   @Autowired
-   public Person(Animal animal, Student student){
-       System.out.println("------------------------- Constructor Injection ================================");
-       System.out.println("constructor injection -  applied in Person constructor for injecting animal dependency");
-       System.out.println(animal + " " + this.animal + " " + student + " " + this.student);
-       this.animal = animal;
-       this.student = student;
-       System.out.println(animal + " " + this.animal + " " + student + " " + this.student);
-   }
+//   @Autowired
+//   public Person(Animal animal, Student student){
+//       System.out.println("------------------------- Constructor Injection ================================");
+//       System.out.println("constructor injection -  applied in Person constructor for injecting animal dependency");
+//       System.out.println(animal + " " + this.animal + " " + student + " " + this.student);
+//       this.animal = animal;
+//       this.student = student;
+//       System.out.println(animal + " " + this.animal + " " + student + " " + this.student);
+//   }
 
+
+    //setter injection for Animal & student both
+    public Animal getAnimal(){return this.animal;}
+    public Student getStudent(){return this.student;}
+
+    @Autowired
+    public void setAnimal(Animal animal){
+        System.out.println("------------------------- Setter Injection -Animal ================================");
+        System.out.println("Setter injection -  applied in Person constructor for injecting animal & student dependency");
+        System.out.println(animal + " " + this.animal);
+        this.animal = animal;
+    }
+
+    @Autowired
+    public void setStudent(Student student){
+        System.out.println("------------------------- Setter Injection - Student ================================");
+        System.out.println("Setter injection -  applied in Person constructor for injecting animal & student dependency");
+        System.out.println(student + " " + this.student);
+        this.student = student;
+    }
 
     public void playWithAnimal(){
         animal.play();
